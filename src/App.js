0a39f4c0e9d199fe46b4313ea906819d5366fd82
Guilherme Rios <guilherme.rc.98@gmail.com>
2019-10-14 22:25:56 -0300

    Componentiza a view

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react'
-import time from './time.js'
 import './App.scss'
+import Counter from './components/Counter.js'
+import Credits from './components/Credits.js'
+import Notifies from './components/Notifies.js'
 
 function App() {
   return (
     <div className="App">
-      <main className="App-counter">
-        <span>Estamos sem bugs há:</span>
-        <h1>12:08:21:45</h1>
-      </main>
-
-      <section className="App-notifies">
-        <div></div>
-        <div></div>
-      </section>
-
-      <footer className="App-credits">
-        <span>
-          Made with &hearts; by <a href="http://github.com/riwersxd" target="_blank">riwersxd</a>
-        </span>
-      </footer>
+      <Counter />
+      <Notifies />
+      <Credits />
     </div>
   )
 }
