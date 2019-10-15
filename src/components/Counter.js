2798824e641bc5e8afe85f6bd53137f724402395
Guilherme Rios <guilherme.rc.98@gmail.com>
2019-10-14 23:56:47 -0300

    Funciona Cronometro

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -9,24 +9,32 @@ class Counter extends React.Component {
     }
   }
 
-  /*this.getSeconds((state) => ({
-    seconds: ('0' + state.timer % 60).slice(-2)
-  }))*/
+  getSeconds() {
+    return ('0' + this.state.timer % 60).slice(-2)
+  }
+  getMinutes() {
+    return ('0' + (Math.floor(this.state.timer / 60) % 60)).slice(-2)
+  }
+  getHours() {
+    return ('0' + (Math.floor(this.state.timer / 3600) % 24)).slice(-2)
+  }
+  getDays() {
+    return ('0' + Math.floor(this.state.timer / 86400)).slice(-2)
+  }
 
-  stopwatch() {
-    setInterval(
-      this.setState((prevState) => ({
+  start() {
+    setInterval(() => {
+      this.setState(prevState => ({
         timer: prevState.timer + 1
-      })),
-      1000
-    )
+      }))
+    },1000)
   }
 
   render() {
     return (
       <main className="counter">
         <span>Estamos sem bugs há:</span>
-        <h1>12:08:21:45</h1>
+        <h1>{this.getDays()}:{this.getHours()}:{this.getMinutes()}:{this.getSeconds()}</h1>
       </main>
     )
   }
