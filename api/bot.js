033b346d005f960323aae7004fc6dca166993a47
riwersxd <guilherme.rc.98@gmail.com>
2019-11-21 12:21:41 -0300

    Não lembro

--- api/bot.js
+++ api/bot.js
@@ -1,38 +1,50 @@
-//
-// var controller = Botkit.slackbot({
-//     debug: true,
-// });
+const { Botkit } = require('botkit');
+const { SlackAdapter } = require('botbuilder-adapter-slack');
+require('dotenv').config();
 
+if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.PORT || !process.env.VERIFICATION_TOKEN) {
+  console.log('Error: Specify CLIENT_ID, CLIENT_SECRET, VERIFICATION_TOKEN and PORT in environment');
+  process.exit(1);
+} else {
+  console.log('Good job, you have the variables!')
+}
 
-// let { Botkit } = require('botkit');
-// const { SlackAdapter } = require('botbuilder-adapter-slack');
-//
-// const adapter = new SlackAdapter({
-//     clientSigningSecret: '5670c2badd0414fc179c7983bfce58ae',
-//     botToken: 'xoxb-778678816625-800578005126-I9XayrpHPKmSSkhc89uz6Dpt'
-// });
-//
-// const controller = new Botkit({
-//     adapter
-// });
-//
-// controller.on('message', async(bot, message) => {
-//     await bot.reply(message, 'I heard a message!');
-// });
-
-// controller.hears(['hello', 'olá', 'oi'],'direct_message, direct_mention, mention', function(bot, message) {
-//     bot.reply(message,'Hello yourself!');
-// });
-
-var SlackBot = require("slackbots");
-var channel = "geral";
-
-var bot = new SlackBot({
-    token: "xoxb-778678816625-800578005126-UnL8Jt6cj8yKvHwkhmrk6b1r",
-    name: "BuglessTimer"
+const adapter = new SlackAdapter({
+     clientSigningSecret: process.env.CLIENT_SIGNING_SECRET,
+     botToken: process.env.BOT_TOKEN
 });
 
-bot.on("start", function() {
-   bot.postMessageToChannel(channel, "Hello world!");
-   console.log("Hello world!");
-})
+const controller = new Botkit({
+     adapter,
+     // ... other configuration options
+});
+
+controller.on('message', async(bot, message) => {
+    console.log('estou ouvindo')
+    await bot.reply(message, 'I heard a message!');
+});
+
+controller.hears('hello','direct_message', function(bot, message) {
+    console.log('estou ouvindo')
+    bot.reply(message,'Hello yourself!');
+});
+
+// var SlackBot = require('slackbots');
+// var channel = 'geral'
+//
+// var bot = new SlackBot({
+//   token: 'xoxb-778678816625-800578005126-UnL8Jt6cj8yKvHwkhmrk6b1r',
+//   name: 'BuglessTimer'
+// })
+//
+//
+// bot.on('message', function(data) {
+//   let message = data.text
+//   // console.log(data.text)
+//   switch (message.toLowerCase()) {
+//     case 'novo bug':
+//     case 'new bug':
+//     bot.postMessageToChannel(channel, 'Bug Registrado!');
+//     console.log('Hello world!');
+//   }
+// })
